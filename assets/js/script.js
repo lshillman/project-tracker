dd4bb83ce13871e893b0f719c4271469e288658e
lshillman <lshillman@users.noreply.github.com>
2022-06-30 13:16:30 -0700

    link bootstrap js to make modal work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 console.log("I'm a JavaScript file linked to this page!");
 
 var now = moment();
+var clock = $("#clock")
 
-$("#clock").text(now.format("MMM Do, YYYY, h:mm:ss a"))
+function updateClock() {
+    clock.text(now.format("MMM Do, YYYY, h:mm:ss a"));
+    console.log("I'm in the updateClock function");
+}
+
+$('#modalButton').click($('#exampleModal').modal());
+
+
+
+$('#projectDeetsModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+setInterval(updateClock, 1000);
+
+
